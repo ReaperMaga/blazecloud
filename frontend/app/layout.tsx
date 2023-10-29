bcc81294d4a5889e52ba3f39f0f263079d653c53
ReaperMaga <reapermaga@gmail.com>
2023-10-29 05:01:44 +0100

    Init frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import '../public/css/globals.css'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import { ReactElement, ReactNode } from 'react'
+
+const roboto = Roboto({ subsets: ['latin'], weight: '400' })
+
+export const metadata: Metadata = {
+  title: 'NextJs Template',
+  description: 'Official Averix NextJS Template',
+}
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: LayoutProps): ReactElement {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
