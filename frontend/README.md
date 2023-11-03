dee24ed82e41814ebf29c851c02bf42a4cfeab62
ReaperMaga <reapermaga@gmail.com>
2023-11-03 05:31:15 +0100

    Added nerd font extension

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# BlazeCloud Frontend
---
+++ frontend/pages/protected.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>I'm protected! Session data: {{ data }}</div>
+    <button class="m-2 rounded-xl p-2 shadow-xl" @click="signOut()">
+      sign out
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ middleware: 'auth' })
+
+const { data, signOut } = useAuth()
+</script>
