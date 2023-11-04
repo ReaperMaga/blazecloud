104dd6dcfed0aec995fbbb6e1a7e3975253012c2
ReaperMaga <reapermaga@gmail.com>
2023-11-04 14:15:16 +0100

    Removed server/api folder, added local provider to nuxt.config.js

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,5 +7,32 @@ export default defineNuxtConfig({
   ],
   typescript: {
     shim: false
+  },
+  auth: {
+    provider: {
+      type: 'local',
+      endpoints: {
+        getSession: {
+          path: '/user'
+        }
+      },
+      pages: {
+        login: '/'
+      },
+      token: {
+        signInResponseTokenPointer: '/token'
+      },
+      sessionDataType: {
+        id: 'string',
+        name: 'string'
+      }
+    },
+    session: {
+      enableRefreshOnWindowFocus: true,
+      enableRefreshPeriodically: 5000
+    },
+    globalAppMiddleware: {
+      isEnabled: true
+    }
   }
 })
